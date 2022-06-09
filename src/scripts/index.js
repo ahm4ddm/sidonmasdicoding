c42f157c6e25fad810eb8efe9f87f50797452086
ahm4ddm <dania9015@gmail.com>
2022-06-09 13:08:38 +0700

    init

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
+import '../styles/style.css';
+import './views/pages/my-nav';
+import './views/pages/my-main';
+import App from './views/app';
+new App();
