9167abb782121b484c5a0e9aed342b2b53358a28
ahm4ddm <dania9015@gmail.com>
2022-06-25 02:37:23 +0700

    finalize app config

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,4 +1,4 @@
-import {Router} from '@vaadin/router';
+import Route from './routes/route';
 import './pages/register';
 import './pages/login';
 import './pages/donation';
@@ -6,16 +6,7 @@ import './pages/about';
 
 class App {
   constructor() {
-    const outlet = document.getElementById('outlet');
-    const router = new Router(outlet);
-    router.setRoutes([
-      {path: '/', component: 'my-main'},
-      {path: '/login', component: 'login-page'},
-      {path: '/register', component: 'register-page'},
-      {path: '/donasi', component: 'donation-page'},
-      {path: '/about', component: 'about-page'},
-      {path: '(.*)', component: 'x-not-found-view'},
-    ]);
+    new Route();
   }
 }
 export default App;
