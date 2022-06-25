bb0fc2f1d5252d1cba5961f52213da81a0fd8940
ahm4ddm <dania9015@gmail.com>
2022-06-25 14:05:49 +0700

    configure weight and height for optimaze

--- src/scripts/views/pages/my-main.js
+++ src/scripts/views/pages/my-main.js
@@ -9,15 +9,15 @@ export class MyMain extends LitElement {
     <main class="bg" id="maincontent">
     <div class="container">
       <div class="row py-5">
-        <div class="col-md">
-        <img class="img-fluid my-5" src="/ilus-landing.png" alt="people send money">
+        <div class="col-md text-center">
+          <img class="my-5" width="300" height="300" src="/ilus-landing.webp" alt="people send money">
         </div>
-        <div class="col-md">
-      <p class="fs-1 fw-bold my-5">Mari Bantu Donasi Masjid</p>
-      <p class="fs-4 text-break">Sekecil apapun yang Anda berikan akan bermanfaat bagi yang membutuhkan</p>
-      <a href="/donasi" target="_blank" rel="noopener noreferrer">
-        <button class="btn btn btn-success fs-3 my-3" type="button">Mulai Donasi </button>
-      <a/>
+      <div class="col-md">
+        <p class="fs-1 fw-bold my-5">Mari Bantu Donasi Masjid</p>
+        <p class="fs-4 text-break">Sekecil apapun yang Anda berikan akan bermanfaat bagi yang membutuhkan</p>
+        <a href="/donasi" target="_blank" rel="noopener noreferrer">
+          <button class="btn btn btn-success fs-3 my-3" type="button">Mulai Donasi </button>
+        <a/>
       </div>
     </div>
     </main>
