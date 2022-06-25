877823708a68c497d0469285c6ed7da0ed6afca1
ahm4ddm <dania9015@gmail.com>
2022-06-25 14:06:29 +0700

    configure proxy

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,25 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    proxy: {
+      '/login': {
+        target: 'http://localhost:7777/app/main',
+        changeOrigin: true,
+      },
+      '/register': {
+        target: 'http://localhost:7777/app/main/auth/register',
+        changeOrigin: true,
+      },
+      '/donasi': {
+        target: 'http://localhost:7777/app/main/donasi',
+        changeOrigin: true,
+      },
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+    static: path.resolve(__dirname, 'dist'),
     historyApiFallback: true,
   },
 });
